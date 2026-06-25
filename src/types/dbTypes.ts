f331b9eac04a3b82f2c9101d31cab2e1ce9b1803
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-06-25 23:53:20 +0200

    refactor: add comments

--- src/types/dbTypes.ts
+++ src/types/dbTypes.ts
@@ -1,3 +1,6 @@
+/**
+ * What the proxy collects after "Try-It-Out" (without `id` and `userId`)
+ */
 export type RequestRecordInput = {
   method: string;
   url: string;
@@ -9,10 +12,16 @@ export type RequestRecordInput = {
   errorDetails?: string;
   timestamp: string;
 };
+/**
+ * What is stored in the database and arrives in History
+ */
 export type RequestRecord = RequestRecordInput & {
   id: string;
   userId: string;
 };
+/**
+ * userId it must be obtained from the function getUserIdFromSession
+ */
 export type SavedSchema = {
   userId: string;
   content: string;
