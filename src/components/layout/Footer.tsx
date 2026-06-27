84db715ba198855e8d3e095ca645a6c7e042dbc3
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-06-27 16:00:38 +0200

    fix: make footer navigation consistent

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,21 +1,24 @@
 import Link from 'next/link';
+import { Button } from '../ui/button';
 
 export default function Footer() {
   return (
     <footer className="bg-background/95 supports-[backdrop-filter]:bg-background/60 w-full border-t backdrop-blur">
       <div className="flex h-16 items-center justify-center gap-10">
-        <Link href="/about" className="text-sm font-medium hover:underline">
-          About
-        </Link>
+        <Button variant="ghost" size="sm" asChild>
+          <Link href="/about">About</Link>
+        </Button>
         <p className="text-sm font-medium">© 2026 Swagger Editor</p>
-        <a
-          href="https://rs.school/courses/reactjs"
-          className="text-sm font-medium hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          RS School
-        </a>
+        <Button variant="ghost" size="sm" asChild>
+          <a
+            href="https://rs.school/courses/reactjs"
+            className="hover:none text-sm font-medium"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            RS School
+          </a>
+        </Button>
       </div>
     </footer>
   );
