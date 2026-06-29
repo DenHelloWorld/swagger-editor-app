ec9ae89d865d8b0319ca4d3ca299dd8abdad23eb
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-06-29 20:32:57 +0200

    feat: add RHF to sign in form

--- src/features/auth/components/SignInForm.tsx
+++ src/features/auth/components/SignInForm.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -7,11 +9,35 @@ import {
   CardDescription,
   CardContent,
 } from '@/components/ui/card';
-import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import Link from 'next/link';
+import { useAuth } from '../useAuth';
+import { Controller, useForm } from 'react-hook-form';
+import { SignInFields, signInSchema } from '../schemas/auth.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function SignInForm() {
+  const { signIn } = useAuth();
+
+  const form = useForm<SignInFields>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  function onSubmit(data: SignInFields) {
+    signIn(data.email);
+  }
+
   return (
     <Card className="w-full sm:max-w-md">
       <CardHeader>
@@ -21,26 +47,49 @@ export default function SignInForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form id="sign-in-form">
+        <form id="sign-in-form" onSubmit={form.handleSubmit(onSubmit)}>
           <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="email">Your email</FieldLabel>
-              <Input
-                id="email"
-                placeholder="example@gmail.com"
-                autoComplete="on"
-              />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input
-                id="password"
-                placeholder="**********"
-                autoComplete="off"
-              />
-            </Field>
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="email">Your email</FieldLabel>
+                  <Input
+                    {...field}
+                    id="email"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="example@gmail.com"
+                    autoComplete="on"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <Input
+                    {...field}
+                    id="password"
+                    placeholder="**********"
+                    autoComplete="off"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
           </FieldGroup>
-          <Button type="submit" form="form-rhf-demo" className="mt-3">
+          <Button type="submit" form="sign-in-form" className="mt-3">
             Submit
           </Button>
         </form>
