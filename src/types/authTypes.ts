e9d116aff7ccc1475c998492b86b9f2ba3f865ec
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-07-01 23:54:31 +0200

    fix: exclude initAuth from UseAuthResult public type

--- src/types/authTypes.ts
+++ src/types/authTypes.ts
@@ -13,6 +13,6 @@ export interface AuthState {
   initAuth: () => Unsubscribe;
 }
 
-export type UseAuthResult = AuthState & {
+export type UseAuthResult = Omit<AuthState, 'initAuth'> & {
   isAuthenticated: boolean;
 };
