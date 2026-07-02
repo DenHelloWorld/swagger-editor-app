9277fa65eca9dc5a5576b252d2d5ea5d0c567d55
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-07-02 22:25:02 +0200

    feat: protect History page from opening in guest mode

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,3 +1,12 @@
-export default function History() {
+import { redirect } from 'next/navigation';
+import { getUserIdFromSession } from '@/lib/auth/getUserIdFromSession';
+
+export default async function History() {
+  const userId = await getUserIdFromSession();
+
+  if (!userId) {
+    redirect('/');
+  }
+
   return <div>History page</div>;
 }
